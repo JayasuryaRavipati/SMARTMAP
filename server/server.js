4c6ccc114ca8748cb6c36d2b7c76acc8508cf21a
23A95A1210 <23a95a1210@aec.edu.in>
2026-08-11 14:12:18 +0530

    Prepare backend for Render

--- server/server.js
+++ server/server.js
@@ -37,8 +37,10 @@ app.use((req, res) => {
   });
 });
 
+
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on Port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
