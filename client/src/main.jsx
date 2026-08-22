94a9dbad92aa2e977c1c43860e9fda7da706ff30
23A95A1210 <23a95a1210@aec.edu.in>
2026-08-22 17:03:01 +0530

    added loader

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,21 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { LoadingProvider } from "./context/LoadingContext";
+
+import Loader from "./components/Loader";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <LoadingProvider>
+                <AuthProvider>
+                    <Loader />
+                    <App />
+                </AuthProvider>
+            </LoadingProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
