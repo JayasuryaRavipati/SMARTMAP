32710fa805093933ec29a73642c74a18c8d83340
23A95A1210 <23a95a1210@aec.edu.in>
2026-08-22 17:06:59 +0530

    changed loader error

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import "../styles/Loader.css";
+
+function Loader() {
+    return (
+        <div className="global-loader">
+            <div className="loader-spinner"></div>
+            <p>Please wait...</p>
+        </div>
+    );
+}
+
+export default Loader;
